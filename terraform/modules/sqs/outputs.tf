b1904933fcf47549d2afcf9829303ed35f9f7c1e
gabriel ferreira flora carvalheiro <gabrielfe1202@gmail.com>
2026-05-07 19:40:28 -0300

    removendo arquivos errados

--- terraform/modules/sqs/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "queue_url" {
-  description = "SQS queue URL"
-  value       = aws_sqs_queue.main.url
-}
-
-output "queue_arn" {
-  description = "SQS queue ARN"
-  value       = aws_sqs_queue.main.arn
-}
